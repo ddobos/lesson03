eb4b7de3a207905484cf3e5e4ae7a9d46e5a1789
Denis Dobos <ddobos40@gmail.com>
2020-05-10 19:23:49 +0300

    add mul and div

--- main.py
+++ main.py
@@ -12,3 +12,24 @@ def inputValue():
                 return a
         except ValueError:
             print('Error input a number, try again')
+
+
+def multiple(firstValue, secondValue):
+    return firstValue * secondValue
+
+
+def divide(firstValue, secondValue):
+    try:
+        return firstValue / secondValue
+    except ZeroDivisionError:
+        print('division by zero')
+
+
+print('Please input first value')
+firstValue = inputValue()
+
+print('Please input second value')
+secondValue = inputValue()
+
+print('multiply result is: {}'.format(multiple(firstValue, secondValue)))
+print('divide result is: {}'.format(divide(firstValue, secondValue)))
