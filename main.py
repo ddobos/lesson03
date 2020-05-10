1fc134b88fce6801037e8260fc100f488403e701
Denis Dobos <ddobos40@gmail.com>
2020-05-10 19:22:32 +0300

    add func sum and dif

--- main.py
+++ main.py
@@ -3,12 +3,32 @@ print('lesson03')
 
 def inputValue():
     print('Please write a number:')
-    while True:
+    a = 0
+    while (True):
         try:
             a = int(input())
-            if a < 0:
+            if (a < 0):
                 print('Error input a number, \n number was by positive')
             else:
-                return a
+                break
         except ValueError:
             print('Error input a number, try again')
+    return a
+
+
+def sum(firstValue, secondValue):
+    return firstValue + secondValue
+
+
+def diff(firstValue, secondValue):
+    return firstValue - secondValue
+
+
+print('Pleas input first value')
+firstValue = inputValue()
+
+print('Pleas input Second value')
+secondValue = inputValue()
+
+print('sum value: {}'.format(sum(firstValue, secondValue)))
+print('difference value: {}'.format(diff(firstValue, secondValue)))
